738161eea89134ef2445bfa9dd828a3512c4e4b0
Silindokuhle Ngubane <silindokuhlengubane0@gmail.com>
2025-02-15 17:48:54 +0200

    FP and readme file

--- actions.js
+++ actions.js
@@ -1,5 +1,6 @@
 /**
  * Action Types
+ * Payloads of information that send data from your application to your store.
  * These constants represent the types of actions that can be dispatched.
  */
 const ADD = "ADD";
@@ -9,18 +10,21 @@ const RESET = "RESET";
 /**
  * Action Creator for ADD action.
  * @returns {Object} The action object for adding 1 to the count.
+ * Action Creator that returns an action object.
  */
 const addAction = () => ({ type: ADD });
 
 /**
  * Action Creator for SUBTRACT action.
  * @returns {Object} The action object for subtracting 1 from the count.
+ * Action creater that returns an action object.
  */
 const subtractAction = () => ({ type: SUBTRACT });
 
 /**
  * Action Creator for RESET action.
  * @returns {Object} The action object to reset the count to 0.
+ * Action creator that returns an action object.
  */
 const resetAction = () => ({ type: RESET });
 
